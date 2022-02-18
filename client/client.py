0031ffd5b744cbde9a0d9e9a6468a3a7a17b5016
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2022-02-18 15:10:22 +0100

    Commiting first initial files.

---
+++ client/client.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("1920x1080")
+window.resizable(0,0)
+window.title("Tremolometer")
+window.config
+window.focus_set()
+
+submit = Button(window, text="Generate", highlightbackground='#3E4149')
+
+
+window.mainloop()
