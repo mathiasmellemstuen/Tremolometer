db579d0f9aeffbf54532c2ba2e2b4f42f583b792
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2022-02-23 02:21:08 +0100

    Added save to XLSX and CSV

--- client/client.py
+++ client/client.py
@@ -1,21 +1,28 @@
-from cgitb import text
 from tkinter import *
-from turtle import color
+from tkinter import messagebox
+from tkinter import filedialog
 from matplotlib import widgets
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.pyplot import xticks
+import pandas as pd
+
+deviceConnected = False
+
 # Window
 window = Tk()
 window.title("Tremolometer")
 window.configure(bg="white")
 
+# List of tuples with x (s) and y (mm) elements
+data = [(1, 1), (2, 2), (3, 2.2), (4, 4), (5, 5)]
+
 figure = Figure(figsize = (19, 5))
 plot = figure.add_subplot(111)
 plot.set_xlabel("Tid (s)")
-plot.set_ylabel("Differanse (mm)")
+plot.set_ylabel("Bevegelse (mm)")
 plot.grid(color='gray', linestyle='dashed')
-plot.plot([])
+plot.plot([element[0] for element in data], [element[1] for element in data], color = (1.0, 0.0, 0.0))
 plot.set_xticks(range(0,21))
 plot.set_xlim([0, 20])
 figure.tight_layout()
@@ -26,20 +33,56 @@ canvas.draw()
 widget = canvas.get_tk_widget()
 widget.grid(column = 1, row = 2 , columnspan=11, sticky = W + E)
 
-startButton = Button(text="Start", padx=10, pady= 10, background="white", foreground="black")
-startButton.grid(column=1, row=1, sticky ="news", padx = 20, pady = 20)
+def dataToCSVString(): 
+    csvString = "\"Tid(s)\",\"Bevegelse (mm)\""
+    for element in data: 
+        csvString += f'\n{element[0]},{element[1]}'
+    
+    return csvString
 
+def start():
+    if deviceConnected: 
+        measuringUI()
+    else: 
+        messagebox.showwarning(title="Ikke tilkoblet", message = "Koble til tremolometer via USB og prøv igjen.")
+
+def saveAsCSV():  
+    f = filedialog.asksaveasfile(title="Lagre som csv", defaultextension = ".csv", filetypes = [("CSV fil", "*.csv")])
+
+    if f is None: 
+        return
+
+    f.write(dataToCSVString())
+    f.close()
+
+def saveAsXLSX():
+    f = filedialog.asksaveasfile(title="Lagre som XLSX", defaultextension = ".xlsx", filetypes = [("Excel fil", "*.xlsx")])
+
+    if f is None: 
+        return
+
+    path = f.name 
+    f.close()
+    dataFrame = pd.DataFrame(data, columns=['Tid(s)', 'Bevegelse (mm)'])
+    dataFrame.to_excel(path, index=False)
+
+# Creating GUI objects
 frequencyLabel = Label(text="Frequency: --Hz", padx=10, pady=10, background="white", foreground="black", anchor="w")
-frequencyLabel.grid(column=1, row=3, sticky="news", padx = 20, pady = 20)
-
-measureLabel = Label(text="Måling: 0 / 20", background="white", foreground="black", anchor="w")
-measureLabel.grid(column=6, row=1, sticky="news")
-
-connectionLabel = Label(text="Tilkoblet", background="white", foreground="green")
-connectionLabel.grid(column=11, row=1, sticky="news")
+measureLabel = Label(text="Måling: 0 / 20 s", background="white", foreground="black", anchor="center")
+connectionLabel = Label(text="Tilkoblet", background="white", foreground="green", anchor="e", padx=25)
+startButton = Button(text="Start", padx=10, pady= 10, background="white", foreground="black", command = start)
+saveCSVButton = Button(text="Lagre som csv", padx=10, pady=10, background="white", foreground="black", command = saveAsCSV)
+saveXLSSButton = Button(text="Lagre som xlsx", padx=10, pady=10, background="white", foreground="black", command = saveAsXLSX)
 
 def disconnectedUI():
-    pass
+
+    connectionLabel.configure(text="Ikke tilkoblet")
+    connectionLabel.configure(foreground="red")
+
+    startButton.grid(column=1, row=1, sticky ="news", padx = 20, pady = 20)
+    frequencyLabel.grid(column=1, row=3, sticky="news", padx = 20, pady = 20)
+    measureLabel.grid(column=6, row=1, sticky="news")
+    connectionLabel.grid(column=11, row=1, sticky="news")
 
 def connectedUI(): 
     pass
@@ -48,21 +91,16 @@ def measuringUI():
     pass
 
 def finishedUI():
-    pass
+    saveCSVButton.grid(column=10, row=3, sticky="news", padx=20, pady=20)
+    saveXLSSButton.grid(column=11, row=3, sticky="news", padx=20, pady=20)
 
 def update():
     canvas.draw()
     window.after(1, update)
 
 
-
-# Packing
-
-# canvas.get_tk_widget().pack()
-# label.pack()
-# startButton.pack()
-
-# Calling the update function for the first time
+disconnectedUI()
+finishedUI()
 update()
 
 window.mainloop()
