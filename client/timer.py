ced3483bc31a21fb27d26d075599f337b555f908
Tobias Hallingstad <tobias@hallingstad.me>
2022-04-15 17:02:47 +0200

    Started work on documenting

--- client/timer.py
+++ client/timer.py
@@ -1,4 +1,14 @@
+"""!
+@package Timer
+Different time functions.
+"""
 import time
 
-def get_current_time_ms():
+
+def get_current_time_ms() -> int:
+    """!
+    Return the rounded time.
+
+    @return Time rounded
+    """
     return round(time.time() * 1000)
