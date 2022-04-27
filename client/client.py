6e3d568e68a0d5652f213edea7027c37fbefc589
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2022-04-27 13:29:13 +0200

    Changes to user interface.

--- client/client.py
+++ client/client.py
@@ -10,8 +10,8 @@ from tkinter.messagebox import showwarning, askquestion
 from timer import get_current_time_ms
 from statistics import mean
 import threading
-import spectrogram
 import numpy as np
+import spectrogram
 import filter
 
 data = []
@@ -114,7 +114,6 @@ def update() -> None:
 
     # Run after the measuring is done
     if measuring and not len(data) == 0 and data[len(data) - 1][0] > config["maaletid"]:
-        interface.finished_ui()
         measuring = False
 
         # Normalize all axis
@@ -148,14 +147,17 @@ def update() -> None:
         for i in range(len(three_axial_length_data) - 1):
             three_axial_length_data[i] = three_axial_length_data[i] - three_axial_length_data_mean
 
-        spectrogram.create_spectrogram_from_data(three_axial_length_data, interface.frequency.plot, config, "hot")
-        spectrogram.create_spectrogram_from_data([d[1] for d in data], interface.frequency_x.plot, config, "hot")
-        spectrogram.create_spectrogram_from_data([d[2] for d in data], interface.frequency_y.plot, config, "hot")
-        spectrogram.create_spectrogram_from_data([d[3] for d in data], interface.frequency_z.plot, config, "hot")
+        strongest_frequency = spectrogram.create_spectrogram_from_data(three_axial_length_data, interface.frequency.plot, config, "hot")
+        strongest_frequency_x = spectrogram.create_spectrogram_from_data([d[1] for d in data], interface.frequency_x.plot, config, "hot")
+        strongest_frequency_y = spectrogram.create_spectrogram_from_data([d[2] for d in data], interface.frequency_y.plot, config, "hot")
+        strongest_frequency_z = spectrogram.create_spectrogram_from_data([d[3] for d in data], interface.frequency_z.plot, config, "hot")
+
         interface.frequency.canvas.draw()
         interface.frequency_x.canvas.draw()
         interface.frequency_y.canvas.draw()
         interface.frequency_z.canvas.draw()
+
+        interface.finished_ui(frequency_all=strongest_frequency, frequency_x=strongest_frequency_x, frequency_y=strongest_frequency_y, frequency_z=strongest_frequency_z)
         interface.update()
 
     interface.window.after(1, update)
--- client/spectrogram.py
+++ client/spectrogram.py
@@ -1,51 +1,49 @@
-from matplotlib import pyplot as plot
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from scipy import signal
 import numpy as np
-
 from typing import List
 from costumeTyping import Data, Config
 
+def calculate_strongest_spectrogram_frequency(Sxx, frequencies_max):
+    row_sums = [sum(row) for row in Sxx]
+    max_sum_index = row_sums.index(max(row_sums))
+    return (frequencies_max * max_sum_index) / len(row_sums)
 
-def calculate_average_spectrogram_data(Sxx, frequencies):
-    mask = np.logical_and(frequencies >= 0, frequencies <=20)
-    average_frequency = Sxx[mask].mean()
-    print(average_frequency)
+def mask_spectrogram(Sxx, mask_percentage):
+    max_value = np.amax(Sxx)
 
-def create_spectrogram_from_data(data: List[Data], graph: Figure, config: Config, cmap_color: str) -> None:
+    for i in range(len(Sxx) - 1):
+        for j in range(len(Sxx[i]) - 1):
+            if Sxx[i][j] / max_value < mask_percentage:
+                Sxx[i][j] = 0
+    return Sxx
+
+def create_spectrogram_from_data(data: List[Data], figure: Figure, config: Config, cmap_color: str) -> None:
     """!
     Create spectrogram figure for the GUI based on the Data measured.
 
     @param data Measurement data.
-    @param graph The given matplotlib figure to use as a spectrogram.
+    @param figure The given matplotlib figure to use as a spectrogram.
     @param config Configuration dict
     """
 
     data_points = np.asarray(data)
     sampling_rate = 1 / 0.025
     frequencies, time, Sxx = signal.spectrogram(x=data_points, fs=sampling_rate, scaling="spectrum", mode="magnitude", nperseg=40, nfft=256)
+
     measuring_time = int(config["maaletid"] / 1000)
+    frequencies_min = int(config["frekvens_min"])
+    frequencies_max = int(config["frekvens_maks"])
+    mask_percentage = int(config["spektrogram_maske"])
 
-    row_sums = [sum(row) for row in Sxx]
-    max_sum_index = row_sums.index(max(row_sums))
+    # Masking the spectrogram to remove lower unwanted values
+    Sxx = mask_spectrogram(Sxx, mask_percentage=mask_percentage)
 
+    figure.set_ylim(frequencies_min, frequencies_max)
+    figure.set_yticks((list(range(frequencies_min, frequencies_max + 1))))
+    figure.set_xlim(0, measuring_time)
+    figure.set_xticks((list(range(0, measuring_time + 1))))
+    figure.pcolormesh(time, frequencies, Sxx, antialiased=False, cmap=cmap_color)
+    figure.axes.grid(color='white', linestyle='dashed')
 
-    frequencies_min = 0
-    frequencies_max = 20
-    energy_minimum_cutoff = 0
-
-    print((frequencies_max / len(row_sums)) * max_sum_index)
-
-    max_value = np.amax(Sxx)
-
-    for i in range(len(Sxx) - 1):
-        for j in range(len(Sxx[i]) - 1):
-            if Sxx[i][j] / max_value < energy_minimum_cutoff:
-                Sxx[i][j] = 0
-
-    graph.set_ylim(frequencies_min, frequencies_max)
-    graph.set_yticks((list(range(frequencies_min, frequencies_max + 1))))
-    graph.set_xlim(0, measuring_time)
-    graph.set_xticks((list(range(0, measuring_time + 1))))
-    graph.pcolormesh(time, frequencies, Sxx, antialiased=False, cmap=cmap_color)
+    return calculate_strongest_spectrogram_frequency(Sxx=Sxx, frequencies_max=frequencies_max)
