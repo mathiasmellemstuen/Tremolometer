8c675a2d498b443ede6bfa0466b20b628516b234
Tobias Hallingstad <tobias@hallingstad.me>
2022-04-28 13:47:18 +0200

    Added some documentation

--- client/config.py
+++ client/config.py
@@ -1,20 +1,43 @@
 """!
 Handling reading and writing data in the configuration yaml file (config.yaml).
 """
+from typing import Any
+
 import yaml
 
 
 class Config:
-    def __init__(self, path):
+    """!
+    Handle config file as a class for more consistent reference in parameters.
+    """
+
+    def __init__(self, path: str) -> None:
+        """!
+        Constructor.
+
+        @param self Pointer to self.
+        @param path Config file path.
+        """
         self.config = dict()
         self.path = path
         self.read()
 
     def read(self) -> None:
+        """!
+        Read config file and store the contents to the local config variable.
+
+        @param self Pointer to self.
+        """
         with open(self.path) as file:
             self.config = yaml.safe_load(file)
 
-    def write(self, data) -> None:
+    def write(self, data: dict) -> None:
+        """!
+        Write contents of data dict to file.
+
+        @param self Pointer to self.
+        @param data Dict with data to store.
+        """
         try:
             file = open(self.path, "w+")
             yaml.dump(data, file, allow_unicode=True, default_flow_style=False)
@@ -23,8 +46,17 @@ class Config:
 
         self.read()
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: str) -> str:
+        """!
+        Square bracket overloading to access elements of config easy.
+
+        @param self Pointer to self.
+        @param item Key to retrieve.
+        """
         return self.config[item]
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: str, value: Any) -> None:
+        """!
+        
+        """
         self.config[key] = value
